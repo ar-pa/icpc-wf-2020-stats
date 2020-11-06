2ae0780ab6427ede673ffd785a1c85fd8a016479
AmirReza PoorAkhavan (Arpa) <a.poorakhavan@gmail.com>
2020-11-06 06:57:11 +0330

    added number of submissions.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import urllib.request
 from math import log10
+from time import time
 
 import tabulate
 
@@ -56,6 +57,36 @@ def get_rating(handle):
         print(e)
 
 
+def get_number_of_submissions(handle):
+    SECONDS_PER_MONTH = 30 * 24 * 60 * 60
+    cached_status = json.loads(open('cached_status', 'r').read())
+    for row in cached_status:
+        if row[0] == handle:
+            return row[1]
+    try:
+        res = json.loads(
+            urllib.request.urlopen("https://codeforces.com/api/user.status?count=1000&handle=" + handle).read())
+        if res['status'] == 'OK':
+            res = res['result']
+            cnt = 0
+            for submission in res:
+                if time() - submission["creationTimeSeconds"] > SECONDS_PER_MONTH:
+                    break
+                if submission["verdict"] == "OK":
+                    cnt += 1
+            cached_status.append([handle, cnt])
+            to_write = open('cached_status', 'w')
+            to_write.write(json.dumps(cached_status))
+            to_write.close()
+            print(handle + " accepted ", cnt)
+            return cnt
+        else:
+            print("user " + handle + " error")
+            return 0
+    except Exception as e:
+        print(e)
+
+
 def get_team_rating(team_members):
     team_members_rating = []
     for member in team_members:
@@ -73,6 +104,9 @@ def get_team_list():
 
 teams = get_team_list()
 for team in teams:
+    if len(team[1]) > 20:
+        team[1] = team[1][0:18] + "..."
     team.append(get_team_rating([team[2], team[3], team[4]]))
+    team.append(sum(get_number_of_submissions(member) for member in team[2:5]))
 teams.sort(key=lambda t: t[5], reverse=True)
 print(tabulate.tabulate(teams, showindex=True))
